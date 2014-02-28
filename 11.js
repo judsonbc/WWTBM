25b363c29bbbaeede0bd90e78318ba905ffa4863
judsonbc <judsonbc@yahoo.com>
2014-02-28 16:05:25 +0800

    wwtbm

---
+++ 11.js
@@ -0,0 +1,50 @@
+
+(function(){var all=document.getElementsByTagName('*');for(var i=0;i<all.length;i++)if(typeof all[i].id!='undefined'&&typeof window[all[i].id]=='undefined')window[all[i].id]=all[i];})()
+browserWarningMessage('Please use Google Chrome, Apple Safari or another supported browser.');
+if (navigator.userAgent.indexOf('iPad') !== -1) {
+  var splashscreen = document.createElement('link');
+  splashscreen.rel = 'apple-touch-startup-image';
+  splashscreen.href = 'C:/Android/WWTBM/images/splashscreen.png';
+  document.getElementsByTagName('head')[0].appendChild(splashscreen);
+}
+
+window.addEventListener('load', function() {
+  Form1.style.display = 'block';
+  NSB.addProperties(Image1);
+
+
+  NSB.addProperties(Answer1);
+
+
+  NSB.addProperties(Answer2);
+
+
+  NSB.addProperties(Answer3);
+
+
+  NSB.addProperties(Answer4);
+
+
+  NSB.addProperties(Question);
+  Form1.style.display = 'none';
+}, false);
+Form1.onsubmit=function(event){window.event.stopPropagation();window.event.preventDefault()};
+NSB.addProperties(Form1);
+function Main() {
+  Image1.Left = 0;
+  Image1.Top = 0;
+  Image1.Width = 320;
+  Image1.Height = 460;
+}
+
+
+window.addEventListener('load', function() {
+  Form1.style.display = 'block';
+  Main();
+  if (typeof(Form1.onshow)=='function') Form1.onshow();
+  
+}, false);
+
+var NSBCurrentForm = Form1;
+
+
---
+++ 9.js
@@ -0,0 +1,8 @@
+
+Form1.innerHTML=[  "<div id='Image1' ><img src='C:/Android/WWTBM/images/bg.png'  height=100% width=100% name='Image1_img' style='' usemap='Image1_map'/></div>",
+  "<label id='Answer1' class='graytitle' style='text-align:left;  font-size:11px; font-family:helvetica; font-style:normal; font-weight:normal; color:white; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >The quick brown fox</label>",
+  "<label id='Answer2' class='graytitle' style='text-align:left;  font-size:11px; font-family:helvetica; font-style:normal; font-weight:normal; color:white; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >The quick brown fox</label>",
+  "<label id='Answer3' class='graytitle' style='text-align:left;  font-size:11px; font-family:helvetica; font-style:normal; font-weight:normal; color:white; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >The quick brown fox</label>",
+  "<label id='Answer4' class='graytitle' style='text-align:left;  font-size:11px; font-family:helvetica; font-style:normal; font-weight:normal; color:white; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >The quick brown fox</label>",
+  "<label id='Question' class='graytitle' style='text-align:left;  font-size:12px; font-family:helvetica; font-style:normal; font-weight:normal; color:white; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >The quick brown fox jumps over the lazy dog near the bank of the river?</label>",
+  ].join('');
