234bf74ffa2658353aa7269ac56cae45076c0c0b
judsonbc <judsonbc@yahoo.com>
2014-03-16 14:03:53 +0800

    2

---
+++ 19.js
@@ -0,0 +1,8 @@
+
+Form3.innerHTML=[  "<div id='Image6' ><img src='images/splashscreen.jpg'  height=100% width=100% name='Image6_img' style='' usemap='Image6_map'/></div>",
+  NSB.TitleBar("TitleBar2", "Congratulation", "", "", "Home,Left", "Home,Right", " style= class=default"),
+  NSB.NavBar_jqm('NavBar4', 'Restart Game', '12', 'helvetica', 'normal', 'bold', 'c', '', 'none', '0', ' '),
+  "<label id='Label1' class='graytitle' style='text-align:center;  font-size:60px; font-family:helvetica; font-style:normal; font-weight:bold; color:RED; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >Winner!!!</label>",
+  "<label id='Label1Copy' class='graytitle' style='text-align:center;  font-size:60px; font-family:helvetica; font-style:normal; font-weight:bold; color:RED; background-color:transparent; border-style:;border-color:transparent;border-width:1px; ' >1,000,000</label>",
+  "<audio id='Audio1' style='' src='winner.wav'    ></audio>",
+  ].join('');
